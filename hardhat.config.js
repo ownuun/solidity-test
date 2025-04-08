dd5a4ab13378f965ac4b945798f82de1187d0ec9
Wonjun Seo <kk01057561299@gmail.com>
2025-04-08 15:36:34 +0900

    첫 컨트랙트 배포

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,5 +2,16 @@ require("@nomicfoundation/hardhat-toolbox");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.28",
+  solidity: {
+    version: "0.8.20",
+    settings: {
+      evmVersion: "paris",
+
+    },
+  },
+  networks: {
+    sepolia: {
+      url: "https://eth-sepolia.g.alchemy.com/v2/42Gohm4VV_jWO1z0cXF_TxaPxp7fl2MV",
+    },
+  },
 };
---
+++ scripts/contractfactory.js
@@ -0,0 +1,21 @@
+const { ethers } = require("hardhat");
+
+async function main() {
+
+    const PRIVATE_KEY = "0x6069ca97bbbde4b6d076597d44eebb29906d357873a8bcba5bbc3c1c3950c496";
+
+    const wallet = new ethers.Wallet(PRIVATE_KEY, ethers.provider);
+    console.log("복원된 지갑 주소:", wallet.address);
+
+    const ContractFactory = await ethers.getContractFactory(
+        "skky_1",
+        wallet
+    );
+
+    const contract = await ContractFactory.deploy(10);
+
+    console.log("컨트랙트 배포 완료: ", contract.target);
+
+}
+
+main();
